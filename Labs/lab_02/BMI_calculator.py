6d9c5be810302fc86d1be136c8e960da67f5b23e
L Fernandez <lovelyfernadez2001@gmail.com>
2022-11-19 23:51:04 +0000

    Updating lab_02 File

---
+++ Labs/lab_02/BMI_calculator.py
@@ -0,0 +1,24 @@
+# Description:
+# Asking user to input height and weight then calculating BMI.
+# Taking input in pounds and converting into metric measurements.
+
+# Taking height and weight input
+weight_pounds = float(input("Please input your weight (Pounds): "))
+
+print("Please input your height (Ft, In): ")
+height_feet = float(input("Feet: "))
+height_inches = float(input("Inches: "))
+
+# Converting pounds into kilograms and inches into meters
+
+height_inches += (height_feet * 12)
+height_meters = height_inches * 0.0254
+weight_kg = weight_pounds * 0.453592
+
+# Calculating BMI
+# Formula: ( weight (kg) / height (metric) ^ 2
+bmi = weight_kg / (height_meters ** 2)
+
+print("Your BMI is : ", round(bmi, 2))
+
+
